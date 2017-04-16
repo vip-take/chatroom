b192ebecc86952efd082423d1e19d9df383ff6b0
Shuhei Taketani <shuhei.taketani@gmail.com>
2017-04-16 11:35:17 +0900

    システム及びARのタイムゾーン設定

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,9 @@ module Chatroom
       g.helper  false
       g.test_framework  nil
     end
+
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
