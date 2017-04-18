5e00b883852312a98e8d09f2c966ef7389e9b22c
Shuhei Taketani <shuhei.taketani@gmail.com>
2017-04-18 11:14:26 +0900

    Revert "userモデルのnameカラムにバリデーション設定"
    This reverts commit cc3402566874efde33c7c58a7db97b890fe6baf7.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  validates :name, null: false, unique: true
 end
